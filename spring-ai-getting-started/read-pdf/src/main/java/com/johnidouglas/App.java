fd3d12ea2c54ffcf1ad1a1688fbb11a0703a989e
Johni Douglas Marangon <johni.marangon@softplan.com.br>
2024-12-16 16:03:57 -0300

    Add Tika integration for PDF text extraction

--- spring-ai-getting-started/read-pdf/src/main/java/com/johnidouglas/App.java
+++ spring-ai-getting-started/read-pdf/src/main/java/com/johnidouglas/App.java
@@ -5,6 +5,8 @@ import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.io.RandomAccessReadBufferedFile;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
+import org.apache.tika.Tika;
+import org.apache.tika.exception.TikaException;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -13,12 +15,24 @@ import java.io.InputStream;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TikaException {
 
         String filename = "/home/johni.marangon@softplan.com.br/Projects/udemy-course-code-repository/spring-ai-getting-started/read-pdf/example.pdf";
-
+        readPDFFileByTika(filename);
         readPDFFileByApachePDFBox(filename);
 
+    }
+
+    private static void readPDFFileByTika(String filePath) throws TikaException {
+
+        try (InputStream stream = new FileInputStream(new File(filePath))) {
+
+            Tika tika = new Tika();
+            String text = tika.parseToString(stream);
+            System.out.println(text);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private static void readPDFFileByApachePDFBox(String filePath) {
